e5b81d47e89689a52dc9b4633de73a3914a05cb6
EC2 Default User <ec2-user@ip-172-31-15-61.ec2.internal>
2021-11-22 06:19:45 +0000

    [Add]layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,32 +1,40 @@
 class BooksController < ApplicationController
   def index
-    @user = User.find(current_user.id)  #@user にログイン中のユーザー情報を代入
-    @books = Book.all
-    @book = Book.new
+    @user = User.find(current_user.id)  #[User Info用]@user にログイン中のユーザー情報を代入
+    @books = Book.all                   #[一覧用]@books に投稿済みbookの全てのデータを代入
+    @book = Book.new                    #[投稿用]@book にbookの投稿機能を代入
+
   end
 
   def show
-    @book = Book.find(params[:id])
+    @user = User.find(current_user.id)  #[投稿user用]@userにログイン中のユーザー情報を代入
+    @book = Book.find(params[:id])      #[book詳細用]@bookに特定のidの情報を代入
   end
 
   def create
-    @book = Book.new(book_params)    #@book にbookの投稿機能を代入
-    @book.user_id = current_user.id  #投稿者のidとbookのidを紐づけ
-    @book.save                       #投稿内容を保存
-    redirect_to book_path(@book.id)  #投稿後、bookのshowページへリダイレクト
+    @book = Book.new(book_params)    #[投稿用]@book にbookの投稿機能を代入
+    @book.user_id = current_user.id  #[投稿用]投稿者のidとbookのidを紐づけ
+    @book.save                       #[投稿用]投稿内容を保存
+    redirect_to book_path(@book.id)  #[投稿用]投稿後、bookのshowページへリダイレクト
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   def destroy
+    book = Book.find(params[:id])   #[削除用]bookのデータを取得
+    book.destroy                    #[削除用]bookのデータを削除
+    redirect_to books_path          #[削除用]book一覧画面へリダイレクト
   end
 
   private
-
 
   def book_params
    params.require(:book).permit(:title, :body)
--- db/migrate/20211121080014_a_dd_profile_image_id_to_users.rb
+++
@@ -1,4 +0,0 @@
-class ADdProfileImageIdToUsers < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
